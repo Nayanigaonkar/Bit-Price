c5a524b3364955242ddfb9535bc1def86653fba7
Nayani <nayanigaonkar4@gmail.com>
2022-08-08 16:33:48 +0530

    Comple App

--- Bit-Price/Controller/ViewController.swift
+++ Bit-Price/Controller/ViewController.swift
@@ -12,15 +12,27 @@ class ViewController: UIViewController {
     @IBOutlet weak var bitcoinLable: UILabel!
     @IBOutlet weak var currencyLable: UILabel!
     @IBOutlet weak var currencyPicker: UIPickerView!
+    @IBOutlet weak var criptoPicker: UIPickerView!
+        
     
     var coinManager = CoinManager()
+    
+    var selectedCurrency = ""
+    var selectedCripto = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         coinManager.delegate = self
+        
         currencyPicker.dataSource = self
         currencyPicker.delegate = self
+        
+        criptoPicker.dataSource = self
+        criptoPicker.delegate = self
+        
+        currencyPicker.tag = 1
+        criptoPicker.tag = 2
     }
 }
 
@@ -46,19 +58,53 @@ extension ViewController: UIPickerViewDataSource, UIPickerViewDelegate {
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
           return 1
-      }
+    }
       
-      func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-          return coinManager.currencyArray.count
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        
+          switch pickerView.tag {
+          case 1:
+              return coinManager.currencyArray.count
+          case 2:
+              return coinManager.criptoArray.count
+          default:
+              return 1
+          }
+          
       }
       
       func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-          return coinManager.currencyArray[row]
+                    
+          switch pickerView.tag {
+          case 1:
+              return coinManager.currencyArray[row]
+          case 2:
+              return coinManager.criptoArray[row]
+          default:
+              return coinManager.currencyArray[row]
+          }
+          
       }
       
       func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-          let selectedCurrency = coinManager.currencyArray[row]
-          coinManager.getCoinPrice(for: selectedCurrency)
+                    
+          switch pickerView.tag {
+          case 1:
+              selectedCurrency = coinManager.currencyArray[row]
+          case 2:
+              selectedCripto = coinManager.criptoArray[row]
+          default:
+              print("pickerView.tag: \(pickerView.tag)")
+          }
+          
+          print("selectedCurrency: \(selectedCurrency)")
+          print("selectedCurrency:  \(selectedCripto)")
+          
       }
+    
+    @IBAction func getPricePressed(_ sender: UIButton) {
+        coinManager.getCoinPrice(for: selectedCurrency, cripto: selectedCripto)
+    }
+    
 }
 
--- Bit-Price/Modal/CoinManager.swift
+++ Bit-Price/Modal/CoinManager.swift
@@ -10,17 +10,20 @@ struct CoinManager {
     
     var delegate: CoinManagerDelegate?
     
-    let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
-    let apiKey = "DCC12D6A-3CB9-4CF8-A47F-265A1B9ECDA5"
+    let baseURL = "https://rest.coinapi.io/v1/exchangerate/"
+    let apiKey = "D10EEF79-7B45-41AC-8CE7-BA2791BC3B21"
 
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
     
-    func getCoinPrice(for currency: String) {
+    let criptoArray = ["BTC", "ETH", "LTC", "PPC", "NMC", "TRC", "DVC", "NVC", "EOS", "XRP", "EOS", "BCH"]
+    
+    func getCoinPrice(for currency: String, cripto: String) {
         
-        let urlString = "\(baseURL)/\(currency)?apikey=\(apiKey)"
+        let urlString = "\(baseURL)\(cripto)/\(currency)?apikey=\(apiKey)"
         
         if let url = URL(string: urlString) {
-            
+            print("URL: \(url)")
+                                    
             let session = URLSession(configuration: .default)
             let task = session.dataTask(with: url) { (data, response, error) in
                 if error != nil {
